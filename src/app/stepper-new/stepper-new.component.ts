a1ddcef62e8e322056c3158ef37b328b3cea2c92
Fernanda Lopez <fernanda.lopez@unosquare.com>
2018-04-10 15:02:25 -0500

    stepper new

--- src/app/stepper-new/stepper-new.component.ts
+++ src/app/stepper-new/stepper-new.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stepper-new',
   templateUrl: './stepper-new.component.html',
-  styleUrls: ['./stepper-new.component.scss']
+  styleUrls: ['./stepper-new.component.scss'],
+  
 })
 export class StepperNewComponent implements OnInit {
 
